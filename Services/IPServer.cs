9b0f7aa64ea0289d8ffc0452f9917c22faf4d5c3
乌龙茶 <982474256@qq.com>
2024-01-16 10:01:53 +0800

    1. 升级依赖 2. 修复断网时程序直接异常退出问题

--- Services/IPServer.cs
+++ Services/IPServer.cs
@@ -15,24 +15,25 @@ namespace neverland.aliyun.ddns.Services
     public class IPServer
     {
         private readonly ILogger _logger;
-        public IPServer(ILogger<IPServer> logger) { 
+        public IPServer(ILogger<IPServer> logger)
+        {
             _logger = logger;
         }
 
         public async Task<string> GetNetworkIPv4(CancellationToken cancelllationToken = new CancellationToken())
         {
-            using (var client = new HttpClient())
+            try
             {
-                client.BaseAddress = new Uri(Contracts.QUERY_IPADDRESS_URL);
-
-                //不使用这个是因为有使用限制,https://ip-api.com/docs/api:json
-                //var query = await client.GetFromJsonAsync<IPModelResult>(Contracts.QUERY_IPADDRESS_RESOURCE, cancelllationToken)
-                //     .ConfigureAwait(false);
-
-                try
+                using (var client = new HttpClient())
                 {
+                    client.BaseAddress = new Uri(Contracts.QUERY_IPADDRESS_URL);
+
+                    //不使用这个是因为有使用限制,https://ip-api.com/docs/api:json
+                    //var query = await client.GetFromJsonAsync<IPModelResult>(Contracts.QUERY_IPADDRESS_RESOURCE, cancelllationToken)
+                    //     .ConfigureAwait(false);
+
                     using var response = await client.GetAsync(Contracts.QUERY_IPADDRESS_RESOURCE, cancelllationToken)
-                     .ConfigureAwait(false);
+                        .ConfigureAwait(false);
                     response.EnsureSuccessStatusCode().WriteRequestToConsole();
                     //检查受限情况
                     var ri = response.Headers.FirstOrDefault(it => it.Key == Contracts.QUERY_IPADDRESS_HEADER_RI).Value;
@@ -56,23 +57,23 @@ namespace neverland.aliyun.ddns.Services
                         }
                     }
                 }
-                catch(ArgumentNullException ex)
-                {
-                    _logger.LogInformation("获取IP错误:{0}", ex.Message);
-                }
-                catch (TaskCanceledException ex) when (ex.InnerException is TimeoutException tex)
-                {
-                    _logger.LogInformation("Timed out: {0}, {1}", ex.Message, tex.Message);
-                }
-                catch (HttpRequestException ex) when (ex is { StatusCode: HttpStatusCode.NotFound })
-                {
-                    // Handle 404
-                    _logger.LogInformation("Not found: {0}", ex.Message);
-                }
-                catch (HttpRequestException ex) when (ex is { StatusCode: null })
-                {
-                    _logger.LogInformation("网络连接失败: {0}", ex.Message);
-                }
+            }
+            catch (ArgumentNullException ex)
+            {
+                _logger.LogInformation("获取IP错误:{0}", ex.Message);
+            }
+            catch (TaskCanceledException ex) when (ex.InnerException is TimeoutException tex)
+            {
+                _logger.LogInformation("Timed out: {0}, {1}", ex.Message, tex.Message);
+            }
+            catch (HttpRequestException ex) when (ex is { StatusCode: HttpStatusCode.NotFound })
+            {
+                // Handle 404
+                _logger.LogInformation("Not found: {0}", ex.Message);
+            }
+            catch (HttpRequestException ex) when (ex is { StatusCode: null })
+            {
+                _logger.LogInformation("网络连接失败: {0}", ex.Message);
             }
             return string.Empty;
         }
